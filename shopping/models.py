4f4638c41dc7a295b54263be3a06423dce051bad
Raul <r.guirao@aragonphotonics.com>
2026-04-07 09:47:48 +0200

    Models, primera funcion del views y url actualizacada

--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    completado = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        ordering = ['completado', '-fecha_creacion']
--- shopping/views.py
+++ shopping/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Producto
 
-# Create your views here.
+def lista_compra(request):
+    if request.method == 'POST':
+        nombre_producto = request.POST.get('nombre')
+        if nombre_producto:
+            Producto.objects.create(nombre=nombre_producto)
+        return redirect('lista_compra')
+    
+    productos = Producto.objects.all()
+    return render(request, 'shopping/lista.html', {'prodcutos': productos})
